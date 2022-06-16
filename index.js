8885afa25b54b4e888e98afc0fbf47656bd566d1
Brady <brady.davenport@pm.me>
2022-06-15 20:15:54 -0600

    server start

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const { sequelize, FoodModel } = require('./src/models');
+const server = require('./src/server');
 
 sequelize.sync()
   .then(() => {
     console.log('Successful Connection');
   })
   .catch(err => console.error(err));
+
+server.start();
