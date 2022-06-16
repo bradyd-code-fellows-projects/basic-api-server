ab87c601ab213a6ac549dc6c95286bf25d603f5c
Brady <brady.davenport@pm.me>
2022-06-15 20:17:34 -0600

    node env

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 const foodSchema = require('./food');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5433/basic-api-server';
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite::memory'
+  : process.env.DATABASE_URL || 'postgres://localhost:5433/basic-api-server';
 
 const sequelize = new Sequelize(DATABASE_URL);
 
