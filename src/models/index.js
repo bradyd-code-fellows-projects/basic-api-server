d756e0f6e36fd74007b3f22b697afc69bd7d7d1f
Brady <brady.davenport@pm.me>
2022-06-15 19:09:58 -0600

    built schema

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+const foodSchema = require('./food');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5433/basic-api-server';
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+const FoodModel = foodSchema(sequelize, DataTypes);
+
+module.exports = {
+  sequelize,
+  FoodModel,
+};
