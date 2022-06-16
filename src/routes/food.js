f3f1a2b7905c9290070517acef18798711bb5b01
Brady <brady.davenport@pm.me>
2022-06-16 17:38:38 -0600

    refactor

--- src/routes/food.js
+++ src/routes/food.js
@@ -1,44 +1,45 @@
 'use strict';
 
 const express = require('express');
-const { FoodModel } = require('../models/food');
+const { FoodModel } = require('../models');
 
 const router = express.Router();
 
-router.get('/food/', async (req, res) => {
+router.get('/food', async (req, res, next) => {
   try {
-    const allRecords = await FoodModel.findAll({});
+    const allRecords = await FoodModel.findAll();
     res.status(200).send(allRecords);
   } catch (e) {
     res.status(404).send('Not found');
   }
 });
 
-router.get('/food/:id', async (req, res) => {
-  const recordId = req.params.id;
+router.get('/food/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const selectedRecord = await FoodModel.findOne({where: {id: recordId}});
+    const selectedRecord = await FoodModel.findOne({where: { id }});
     res.status(200).send(selectedRecord);   
   } catch (e) {
     res.status(404).send('Not found');
   }
 });
 
-router.post('/food', async (req, res) => {
+router.post('/food', async (req, res, next) => {
   const food = req.body;
   try {
     let response = await FoodModel.create(food);
+    console.log('response: ', response);
     res.status(200).send(response);
   } catch (e) {
     res.status(404).send('Cannot perform this method');
   }
 });
 
-router.put('/food/:id', async (req, res) => {
-  const recordId = req.params.id;
+router.put('/food/:id', async (req, res, next) => {
+  const { id } = req.params;
   const updatedRecord = req.body;
   try {
-    const selectedRecord = await FoodModel.findOne({where: {id: recordId}});
+    const selectedRecord = await FoodModel.findOne({where: { id }});
     await selectedRecord.update(updatedRecord);
     await selectedRecord.save();
     res.status(200).send(selectedRecord);
@@ -47,10 +48,10 @@ router.put('/food/:id', async (req, res) => {
   }
 });
 
-router.delete('/food/:id', async (req, res) => {
-  const recordId = req.params.id;
+router.delete('/food/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const recordToDelete = await FoodModel.findOne({where: {id: recordId}});
+    const recordToDelete = await FoodModel.findOne({where: { id }});
     await recordToDelete.destroy();
     res.status(200).send(recordToDelete);
   } catch (e) {
