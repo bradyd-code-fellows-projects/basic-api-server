c628463ee9a69d629f9ae3f714a00ab031a29770
Brady <brady.davenport@pm.me>
2022-06-16 17:37:52 -0600

    file navigation

--- src/models/index.js
+++ src/models/index.js
@@ -2,17 +2,30 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
-const foodSchema = require('./food');
+const foodSchema = require('./food.schema');
+const vehicleSchema = require('./vehicle.schema');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
-  : process.env.DATABASE_URL || 'postgres://localhost:5433/basic-api-server';
+  : process.env.DATABASE_URL || 'postgres://localhost:5432/basic-api-server';
 
-const sequelize = new Sequelize(DATABASE_URL);
+// const sequelize = new Sequelize(DATABASE_URL);
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const FoodModel = foodSchema(sequelize, DataTypes);
+
+const VehicleModel = vehicleSchema(sequelize, DataTypes);
 
 module.exports = {
   sequelize,
   FoodModel,
+  VehicleModel,
 };
