a9ce17f399e394811564d79ff0f32693c5baf4fd
Brady <brady.davenport@pm.me>
2022-06-16 17:38:12 -0600

    refactor

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-require('dotenv').config();
 const logger = require('./middleware/logger');
 const foodRoute = require('./routes/food');
+const vehicleRoute = require('./routes/vehicle');
 const notFound = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
 
-const PORT = process.env.PORT || 3002;
-
+const app = express();
+app.use(logger);
 app.use(express.json());
 
-app.use(logger);
+app.use(foodRoute);
+app.use(vehicleRoute);
+
+const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
   res.status(200).send('This is the server for Brady\'s 401-lab03');
 });
-
-app.use(foodRoute);
 
 app.use('*', notFound);
 app.use(errorHandler);
